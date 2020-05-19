67f69895617c05d07532cef5dd2564043a318b69
Jayesh Srivastava <jayesh0200@gmail.com>
2020-05-20 02:55:12 +0530

    Create staircase.java

---
+++ Problem-Solving/staircase.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for(int a=0;a<n;a++)
+        {
+            for(int b=n-a;b>1;b--)
+            {
+                System.out.print(" ");
+            }
+            for(int b=0;b<=a;b++)
+            {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
