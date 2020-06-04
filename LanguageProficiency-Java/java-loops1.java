736200cb562345a9dcd7b31cd8a47c9f23ab0777
Jayesh Srivastava <jayesh0200@gmail.com>
2020-06-04 14:53:25 +0530

    Create java-loops1.java

---
+++ LanguageProficiency-Java/java-loops1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Abc
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int x=1;x<=10;x++)
+        {
+            System.out.println(a+" x "+x+" = "+(a*x));
+        }
+
+    }
+}
